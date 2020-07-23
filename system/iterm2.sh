47c6e880d695c778429f5aab4ed6fc9aae63b62a
ka2er <personseb+git@gmail.com>
2020-07-23 23:26:08 +0200

    FIX : iterm2 no warn if .iterm2 exists

--- system/iterm2.sh
+++ system/iterm2.sh
@@ -3,8 +3,10 @@
 # iterm 2
 # http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
 
-mkdir $HOME/.iterm2
+mkdir -p $HOME/.iterm2
 cp dotfiles/com.googlecode.iterm2.plist $HOME/.iterm2
+
+fi
 # Specify the preferences directory
 defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.iterm2"
 # Tell iTerm2 to use the custom preferences in the directory
