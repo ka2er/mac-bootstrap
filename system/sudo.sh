bf27d87fcd4b1f0da4df5d64a6ab42a0c6cd2568
ka2er <personseb+git@gmail.com>
2020-07-24 21:41:14 +0200

    ENH : sudo patch only if needed

--- system/sudo.sh
+++ system/sudo.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-sudo patch -u -b /etc/pam.d/sudo -i patch/pam.d-sudo.patch
+# T2 : sudo
+grep tid /etc/pam.d/sudo
+if [ $? -eq 0 ]; then
+    echo "Sudo already allow T2 password input"
+else
+    echo "Adding support for T2 for sudo"
+    sudo patch -u -b /etc/pam.d/sudo -i patch/pam.d-sudo.patch
+fi
