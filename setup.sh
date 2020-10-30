85ba19a0ca06b4b2ecf1235c92117a58f2ee4b73
ka2er <personseb+git@gmail.com>
2020-10-30 19:14:05 +0100

    ENH : brew cask ls deprecated

--- setup.sh
+++ setup.sh
@@ -45,7 +45,7 @@ sudo chmod u+s /usr/local/Cellar/htop/*/bin/htop
 
 # casks
 for pkg in spectacle bitwarden macupdater homebrew/cask-fonts/font-meslo-lg-nerd-font iterm2 notion whatsapp google-chrome fanny github visual-studio-code; do
-    if brew cask ls --versions $pkg > /dev/null; then
+    if brew list --cask --versions $pkg > /dev/null; then
         # The package is installed
         echo "$pkg already installed. Skipping"
     else
