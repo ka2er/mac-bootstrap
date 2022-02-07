01c2ffdaa538604931d63f44a8949bfa4fc6dd59
ka2er <personseb@wospe.fr>
2022-02-07 23:01:20 +0100

    NEW : gpg + chezmoi

--- setup.sh
+++ setup.sh
@@ -29,7 +29,7 @@ cp dotfiles/.p10k.zsh $HOME/
 
 #
 # brew package
-for pkg in ykman htop zsh-autosuggestions tree pstree pidof openssh lnav ncdu mas bitwarden-cli; do
+for pkg in gpg pinentry-mac ykman htop zsh-autosuggestions tree pstree pidof openssh lnav ncdu mas chezmoi bitwarden-cli; do
     if brew ls --versions $pkg > /dev/null; then
         # The package is installed
         echo "$pkg already installed. Skipping"
